fb74f5a5eab209f51904bb0844fc2ebd1ec85e4e
Harsh9307 <diwaseharsh@gmail.com>
2025-02-03 16:50:51 +0530

    javascript

--- let_block_scoping_2.js
+++ let_block_scoping_2.js
@@ -1,16 +1,17 @@
 var teacher = "Sanket";   // gloabal scope    teacher -> sanket
 function fun(){
+    console.log(teacher);
     var teacher = "Pulkit";    // fun scope
     let content ="JS";         // fun scope
     if(content== "JS"){
         let hours = "120+";       // block scope
-        console.log(hours);
+        console.log(content,hours);
     }
     console.log(teacher,content);
 }
 fun();
 console.log(teacher);
-console.log(content);
+//console.log(content);
 
 
 // 120+ ,Pulkit , JS
